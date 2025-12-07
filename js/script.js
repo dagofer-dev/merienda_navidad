811ecd462e440dac7197fabc6155d07fe00feea0
dagofer-dev <45543438+dagofer-dev@users.noreply.github.com>
2025-12-07 11:54:23 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -49,6 +49,69 @@ function verificarRespuesta(pregunta) {
             div_desaparece = "div_p3";
             div_aparece = "div_continuar";
             break;
+        case "pregunta4":
+            respuestaCorrecta = "resp_prin2";
+            img_difuminar = "img_principales";
+            difuminado = "blur(6px)";
+            div_desaparece = "div_p1";
+            div_aparece = "div_p2";
+            break;
+        case "pregunta5":
+            respuestaCorrecta = "resp_prin5";
+            img_difuminar = "img_principales";
+            difuminado = "blur(3px)";
+            div_desaparece = "div_p2";
+            div_aparece = "div_p3";
+            break;
+        case "pregunta6":
+            respuestaCorrecta = "resp_prin11";
+            img_difuminar = "img_principales";
+            difuminado = "blur(0px)";
+            div_desaparece = "div_p3";
+            div_aparece = "div_continuar";
+            break;
+        case "pregunta7":
+            respuestaCorrecta = "resp_postre3";
+            img_difuminar = "img_postres";
+            difuminado = "blur(5px)";
+            div_desaparece = "div_p1";
+            div_aparece = "div_p2";
+            break;
+        case "pregunta8":
+            respuestaCorrecta = "resp_postre8";
+            img_difuminar = "img_postres";
+            difuminado = "blur(0px)";
+            div_desaparece = "div_p2";
+            div_aparece = "div_continuar";
+            break;
+        case "pregunta9":
+            respuestaCorrecta = "resp_bebida4";
+            img_difuminar = "img_bebidas";
+            difuminado = "blur(9px)";
+            div_desaparece = "div_p1";
+            div_aparece = "div_p2";
+            break;
+        case "pregunta10":
+            respuestaCorrecta = "resp_bebida7";
+            img_difuminar = "img_bebidas";
+            difuminado = "blur(6px)";
+            div_desaparece = "div_p2";
+            div_aparece = "div_p3";
+            break;
+        case "pregunta11":
+            respuestaCorrecta = "resp_bebida10";
+            img_difuminar = "img_bebidas";
+            difuminado = "blur(3px)";
+            div_desaparece = "div_p3";
+            div_aparece = "div_p4";
+            break;
+        case "pregunta12":
+            respuestaCorrecta = "resp_bebida9";
+            img_difuminar = "img_bebidas";
+            difuminado = "blur(0px)";
+            div_desaparece = "div_p4";
+            div_aparece = "div_continuar";
+            break;
         default:
             respuestaCorrecta = "ninguna";
     }
